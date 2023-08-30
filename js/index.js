ac27e74fd3b8af5ce7bd9cff14902d573b5ff606
Saif-Rahman-801 <saifurs102@gmail.com>
2023-08-30 19:24:46 +0600

    data shown

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
 const handleNews = async () => {
-  const data = await fetch(
+  const response = await fetch(
     " https://openapi.programming-hero.com/api/news/categories"
   );
-  const response = await data.json();
-  console.log(response);
-  
+  const data = await response.json();
+  //   console.log(data);
+  console.log(data.data.news_category);
+  const newsCategory = data.data.news_category;
+  const linkContainer = document.getElementById("link-container");
+  newsCategory.slice(0, 4).forEach((singleNewsCategory) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <a class="tab font-semibold text-2xl">${singleNewsCategory.category_name}</a>
+    `;
+    linkContainer.appendChild(div);
+  });
 };
 handleNews();
